c2419397cc8793a98c53def52fcf1884fe0c46d5
unknown <shitalisurnar@gmail.com>
2024-07-18 23:08:34 +0530

    framework created by shital team leader

---
+++ src/main/java/BaseLayer/BaseClass.java
@@ -0,0 +1,14 @@
+package BaseLayer;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+	
+	public static void inilization()
+	{
+		WebDriver Driver = new ChromeDriver();
+		
+	}
+
+}
